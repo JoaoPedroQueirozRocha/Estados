197cf18ae0656e3e605bb24cdaceaee4bc20fb42
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2022-06-20 16:54:42 -0300

    working 1

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import {promises  as fs} from "fs";
+import { promises as fs } from "fs";
 
-init()
+init();
 
-async function init(){
+async function init() {
     await getStatesWithMoreCities();
     await createFile();
-    await pontosTuristicosPorCapital();
 }
-  
 
-
-
-async function createFile(){
-    let data = await fs.readFile(`./files/Estados.json`);
-    let states = JSON.parse(data);
-    
-    data = await fs.readFile(`./files/Cidades.json`);
-    let cities = JSON.parse(data);
-    
-    
-
-    for(let state of states){
-        const stateCities = cities.filter(city => city.Estado === state.ID);
-        await fs.writeFile(`./states/${state.Sigla}.json`, JSON.stringify(stateCities));
+async function createFile() {
+    let states = JSON.parse(await fs.readFile(`./files/Estados.json`));
+    let cities = JSON.parse(await fs.readFile(`./files/Cidades.json`));
+    let capitals = JSON.parse(await fs.readFile(`./files/Capitais.json`));
+    create(states, cities, capitals);
 }
+
+async function create(states, cities, capitals) {
+    for (let state of states) {
+        const stateCities = cities.filter((city) => city.Estado === state.ID);
+        await fs.writeFile(
+            `./states/${state.Sigla}.json`,
+            JSON.stringify(stateCities)
+        );
+        await AgruparPorRegioes(state.Sigla, states, capitals, cities);
+    }
+    console.log(cities.length);
 }
 
 async function contagem(uf) {
@@ -36,25 +36,117 @@ async function getStatesWithMoreCities() {
     const states = JSON.parse(await fs.readFile("./files/Estados.json"));
     const list = [];
 
-    for (let state of states){
+    for (let state of states) {
         const count = await contagem(state.Sigla);
-        list.push({uf: state.Sigla, count});
-
+        list.push({ uf: state.Sigla, count });
     }
 
-    list.sort((a, b)=> {
+    list.sort((a, b) => {
         if (a.count < b.count) return 1;
-        else if(a.count > b.count) return -1;
-        
-    })
+        else if (a.count > b.count) return -1;
+    });
 
     const resultado = [];
 
-        list.slice(0, 5).forEach(item => resultado.push(item.uf + " - " + item.count))
-    console.log(resultado)
+    list
+        .slice(0, 5)
+        .forEach((item) => resultado.push(item.uf + " - " + item.count));
+    console.log(resultado);
 }
 
-
-function pontosTuristicosPorCapital() {
-    
+async function AgruparPorRegioes(uf, states, capitals, cities) {
+    try {
+        for (let capital of capitals) {
+            if (capital.Sigla === uf) {
+                if (capital.Regiao == "Norte") {
+                    for (let state of states) {
+                        if (state.Sigla === uf) {
+                            await fs.writeFile(
+                                `./regioes/Norte/Estados/${uf}.json`,
+                                JSON.stringify(state)
+                            );
+                            for (let cidade of cities) {
+                                if (cidade.Estado === state.ID && capital.Regiao == "Norte") {
+                                    await fs.writeFile(
+                                        `./regioes/Norte/Cidades/${cidade.Nome}.json`,
+                                        JSON.stringify(cidade)
+                                    );
+                                }
+                            }
+                        }
+                    }
+                } else if (capital.Regiao == "Nordeste") {
+                    for (let state of states) {
+                        if (state.Sigla == uf) {
+                            await fs.writeFile(
+                                `./regioes/Nordeste/Estados/${uf}.json`,
+                                JSON.stringify(state)
+                            );
+                            for (let cidade of cities) {
+                                if (cidade.Estado == state.ID) {
+                                    await fs.writeFile(
+                                        `./regioes/Nordeste/Cidades/${cidade.Nome}.json`,
+                                        JSON.stringify(cidade)
+                                    );
+                                }
+                            }
+                        }
+                    }
+                } else if (capital.Regiao == "Sul") {
+                    for (let state of states) {
+                        if (state.Sigla == uf) {
+                            await fs.writeFile(
+                                `./regioes/Sul/Estados/${uf}.json`,
+                                JSON.stringify(state)
+                            );
+                            for (let cidade of cities) {
+                                if (cidade.Estado == state.ID) {
+                                    await fs.writeFile(
+                                        `./regioes/Sul/Cidades/${cidade.Nome}.json`,
+                                        JSON.stringify(cidade)
+                                    );
+                                }
+                            }
+                        }
+                    }
+                } else if (capital.Regiao == "Centro-Oeste") {
+                    for (let state of states) {
+                        if (state.Sigla == uf) {
+                            await fs.writeFile(
+                                `./regioes/Centro-Oeste/Estados/${uf}.json`,
+                                JSON.stringify(state)
+                            );
+                            for (let cidade of cities) {
+                                if (cidade.Estado == state.ID) {
+                                    await fs.writeFile(
+                                        `./regioes/Centro-Oeste/Cidades/${cidade.Nome}.json`,
+                                        JSON.stringify(cidade)
+                                    );
+                                }
+                            }
+                        }
+                    }
+                } else if (capital.Regiao == "Sudeste") {
+                    for (let state of states) {
+                        if (state.Sigla == uf) {
+                            await fs.writeFile(
+                                `./regioes/Sudeste/Estados/${uf}.json`,
+                                JSON.stringify(state)
+                            );
+                            for (let cidade of cities) {
+                                if (cidade.Estado == state.ID) {
+                                    await fs.writeFile(
+                                        `./regioes/Sudeste/Cidades/${cidade.Nome}.json`,
+                                        JSON.stringify(cidade)
+                                    );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
